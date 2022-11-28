951e31d702e0b47d4b16cf732a24f585623e78fb
SVDmitriev <git.user.1989@gmail.com>
2022-11-28 19:01:27 +0300

    add prog

--- src/main/java/ru/access/parser/Main.java
+++ src/main/java/ru/access/parser/Main.java
@@ -6,8 +6,26 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Введите текст и нажмите <Enter>: " );
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число 1:" );
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите число 2:" );
+        int number2 = new Scanner(System.in).nextInt();
+
+        //расчет суммы
+        int sum= number1+number2;
+        System.out.println("Сумма = " +sum );
+
+        //расчет разности
+        int raz= number1-number2;
+        System.out.println("Разность = " +raz);
+        //расчет произведения
+        int ymn= number1*number2;
+        System.out.println("Произведение = "+ymn );
+
+        //расчет деления
+        double chas= (double)number1/number2 ;
+        System.out.println("Частное = " +chas );
+
+
     }
 }
